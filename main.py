fe5e32fdb8b8f409bd382add060f925fad3741e8
Raylan <raylanwork@gmail.com>
2023-09-28 18:34:53 -0300

    Listando nfes

--- main.py
+++ main.py
@@ -20,71 +20,21 @@ def get_session():
         yield session
     finally:
         session.close()
- 
-@app.get("/")
-def root():
-    return "nfes"
- 
-@app.post("/nfe", response_model=schemas.Nfe, status_code=status.HTTP_201_CREATED)
-def create_nfe(nfe: schemas.NfeCreate, session: Session = Depends(get_session)):
- 
-    nfedb = models.Nfe(chNFe = nfe.chNFe)
- 
-    session.add(nfedb)
-    session.commit()
-    session.refresh(nfedb)
- 
-    return nfedb
- 
-@app.get("/nfe/{id}", response_model=schemas.Nfe)
-def read_nfe(id: int, session: Session = Depends(get_session)):
- 
-    nfe = session.query(models.Nfe).get(id) # get item with the given id
- 
-    # check if id exists. If not, return 404 not found response
-    if not nfe:
-        raise HTTPException(status_code=404, detail=f"nfe item with id {id} not found")
- 
-    return nfe
- 
-@app.put("/nfe/{id}", response_model=schemas.Nfe)
-def update_nfe(id: int, task: str, session: Session = Depends(get_session)):
- 
-    nfe = session.query(models.Nfe).get(id)     # get given id
- 
-    if nfe:
-        nfe.task = task
-        session.commit()
- 
-    # check if id exists. If not, return 404 not found response
-    if not nfe:
-        raise HTTPException(status_code=404, detail=f"nfe item with id {id} not found")
- 
-    return nfe
- 
-@app.delete("/nfe/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_nfe(id: int, session: Session = Depends(get_session)):
- 
-    # get the given id
-    nfe = session.query(models.Nfe).get(id)
- 
-    # if nfe item with given id exists, delete it from the database. Otherwise raise 404 error
-    if nfe:
-        session.delete(nfe)
-        session.commit()
-    else:
-        raise HTTPException(status_code=404, detail=f"nfe item with id {id} not found")
- 
-    return None
- 
-@app.get("/nfe", response_model = List[schemas.Nfe])
-def read_nfe_list(session: Session = Depends(get_session)):
- 
-    nfe_list = session.query(models.Nfe).all() # get all nfe items
- 
-    return nfe_list  
 
-@app.post("/upload", status_code=status.HTTP_201_CREATED)
+@app.get("/nfe/", response_model=List[schemas.Nfe])
+def read_nfes(session: Session = Depends(get_session)):
+    nfes = session.query(models.Nfe).all()
+    return nfes
+
+@app.get("/nfe/{nfe_id}")
+def get_nfe_details(nfe_id: int, session: Session = Depends(get_session)):
+    nfe = session.query(models.Nfe).filter(models.Nfe.id == nfe_id).first()
+
+    if not nfe:
+        raise HTTPException(status_code=404, detail="NFE não encontrada")
+    return nfe
+
+@app.post("/upload/", status_code=status.HTTP_201_CREATED)
 async def upload(file:UploadFile = File(...), session: Session = Depends(get_session)):
     try:
         xml_data = await file.read()
--- schemas.py
+++ schemas.py
@@ -1,13 +1,66 @@
 from pydantic import BaseModel
+from datetime import datetime
+from decimal import Decimal
  
 # Create Nfe Schema (Pydantic Model)
-class NfeCreate(BaseModel):
-    task: str
+# class NfeCreate(BaseModel):
+#     chNFe: str
+#     UF: str
+#     cStat: str
+#     mod: str
+#     dhEmi: datetime
+#     dhSaiEnt: datetime
+#     tpNF: int
+#     cMunFG: str
+#     tpEmis: str
+#     tpAmb: str
+#     vBC: Decimal
+#     vICMS: Decimal
+#     vICMSDeson: Decimal
+#     vBCST: Decimal
+#     vST: Decimal
+#     vProd: Decimal
+#     vFrete: Decimal
+#     vSeg: Decimal
+#     vDesc: Decimal
+#     vII: Decimal
+#     vPIS: Decimal
+#     vCOFINS: Decimal
+#     vNF: Decimal
+#     vTotTrib: Decimal
  
 # Complete Nfe Schema (Pydantic Model)
 class Nfe(BaseModel):
     id: int
-    task: str
+    chNFe: str
+    nProt: str
+    UF: str
+    cStat: str
+    cNF: str
+    natOp: str
+    mod: str
+    serie: str
+    nNF: str
+    dhEmi: datetime
+    dhSaiEnt: datetime
+    tpNF: int
+    cMunFG: str
+    tpEmis: str
+    tpAmb: str
+    vBC: Decimal
+    vICMS: Decimal
+    vICMSDeson: Decimal
+    vBCST: Decimal
+    vST: Decimal
+    vProd: Decimal
+    vFrete: Decimal
+    vSeg: Decimal
+    vDesc: Decimal
+    vII: Decimal
+    vPIS: Decimal
+    vCOFINS: Decimal
+    vNF: Decimal
+    vTotTrib: Decimal
  
     class Config:
         orm_mode = True
