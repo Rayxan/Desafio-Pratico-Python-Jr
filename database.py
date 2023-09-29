010c6dddb21516d204383e70b807a965743c63b8
Raylan <raylanwork@gmail.com>
2023-09-28 22:10:53 -0300

    Setting up the project

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
-engine = create_engine("mysql+pymysql://root@localhost:3312/test")
+engine = create_engine("mysql+pymysql://root@localhost:3306/desafio")
  
 Base = declarative_base()
 
